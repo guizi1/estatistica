e75cbeb0b13d179d4d2ab0408b13b5923bc4cf7b
EFG <efg@efg.local>
2025-06-09 21:14:20 -0300

    Atualização 09/06 uso Fake

---
+++ distribuicao_frequencia.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import numpy as np
+import math
+
+def analisar_dados_estatisticos(dados_brutos, nome_do_conjunto):
+    print(f"--- Análise Estatística para: {nome_do_conjunto} ---")
+
+    # 1. Rol (Dados Ordenados)
+    rol = sorted(dados_brutos)
+    print("\n1. Rol (Dados Ordenados):")
+    print(f" {rol}")
+
+    # 2. Tamanho da Amostra (n)
+    n = len(rol)
+    print("\n2. Tamanho da Amostra (n):")
+    print(f" n = {n}")
+
+    # 3. Valor Máximo e Mínimo
+    x_min = rol[0]
+    x_max = rol[-1]
+    print("\n3. Valor Máximo e Mínimo:")
+    print(f" valor min: {x_min}")
+    print(f" valor max: {x_max}")
+
+    # 4. Amplitude Total (AT)
+    at = x_max - x_min
+    print("\n4. Amplitude Total (AT): ")
+    print(f"AT = {at:.2f}")
+
+    # 5. Número de Classes (K)
+    k = math.ceil(math.sqrt(n))
+    print("\n5. Número de Classes (K):")
+    print(f"K = {k}")
+    
+    # 6. Amplitude das Classes (h)
+    h = at / k
+    print('\n6. Amblitude das Classes (h): ')
+    print(f'H = {h}')
+
+    classes = []
+    frequencias_absolutas = []
+    pontos_medios = []
+    frequencias_relativas_dec = []
+    frequencias_relativas_perc = []
+    frequencias_absolutas_acum = []
+
+    frequencia_abs_acum = 0
+    limite_inferior = x_min
+    for i in range(k):
+        limite_superior = limite_inferior + h 
+        # Classes
+        classes.append(f'[{limite_inferior} |-- {limite_superior}]')
+        # Frequência Absoluta
+        if i == k - 1:
+            frequencia_absoluta = len([x for x in rol if limite_inferior <= x <= limite_superior])
+        else:
+            frequencia_absoluta = len([x for x in rol if limite_inferior <= x < limite_superior])
+
+        frequencias_absolutas.append(frequencia_absoluta)
+        # Pontos Médio da classe
+        pontos_medios.append((limite_inferior + limite_superior) / 2)
+        # Relativa Decimal
+        frequencias_relativas_dec.append(frequencias_absolutas[i] / n)
+        # Relativa Absoluta Acumulada
+        frequencia_abs_acum = frequencia_abs_acum + frequencias_absolutas[i]
+        frequencias_absolutas_acum.append(frequencia_abs_acum)
+        # Relativa Percentual
+        frequencias_relativas_perc.append(frequencias_relativas_dec[i] * 100)
+        limite_inferior = limite_superior
+
+    df_frequencia = pd.DataFrame({
+        'Classe': classes, 
+        'Ponto Médio': pontos_medios,
+        'Frequência Absoluta': frequencias_absolutas,
+        'Frequência Relativa Decimal': frequencias_relativas_dec,
+        'Frequência Relativa Percentual (%)': frequencias_relativas_perc,
+        'Frequência Absoluta Acumulada': frequencias_absolutas_acum
+    })
+
+    df_frequencia.loc['Total'] = [
+        'Total',
+        np.nan,
+        df_frequencia['Frequência Absoluta'].sum(),
+        df_frequencia['Frequência Relativa Decimal'].sum(),
+        df_frequencia['Frequência Relativa Percentual (%)'].sum(),
+        np.nan
+    ]
+    return df_frequencia
+
+
+
+    '''
+    # Arredondar amplitude das classes
+    num_casas_decimais = 0
+    if at < k: # 0.1
+        num_casas_decimais = 1
+        if at * 10 < k: # 0.01
+            num_casas_decimais = 2
+    h = np.ceil(h * (10**num_casas_decimais)) / (10**num_casas_decimais)
+    print("\n6. Amplitude das Classes (h):")
+    print(f"h = {int(h)}")
+    '''
+
+# --- ESTUDO DE CASO 01: IDADE DOS ALUNOS
+dados_idades = [21.0, 21, 21, 22, 22, 22, 23, 23, 23, 23, 24, 24, 25, 25, 25, 26, 27, 27, 28, 28, 30, 30, 31, 31, 31]
+df_idades = analisar_dados_estatisticos(dados_idades, "Idades dos Alunos")
+print(df_idades.to_string())
+
+dados_call_center = [5.2, 8.1, 6.5, 12.0, 7.3, 5.8, 9.5, 11.2, 6.0, 7.8, 5.5, 10.1, 6.7, 7.0, 8.5, 13.5, 6.2, 7.5, 9.0, 10.5, 5.0, 8.8, 6.9, 7.2, 11.5, 6.3, 9.8, 10.0, 7.6, 8.0]
+df_call_center = analisar_dados_estatisticos(dados_call_center, "Tempo de Atendimento em Call Center")
+print(df_call_center.to_string())
---
+++ media_ponderada.py
@@ -0,0 +1,80 @@
+# Dados de temperatura de cada sensor (25 leituras no total)
+# Sensor A (10 leituras - Mais Preciso)
+sensor_a_data = [20.1, 20.3, 20.0, 20.2, 20.1, 20.3, 20.0, 20.2, 20.1, 20.3]
+# Sensor B (10 leituras - Precisão Média)
+sensor_b_data = [20.5, 20.0, 21.0, 20.2, 20.8, 20.1, 20.6, 20.3, 20.9, 20.4]
+# Sensor C (5 leituras - Menos Preciso)
+sensor_c_data = [21.5, 19.8, 22.0, 20.0, 19.5]
+
+
+# todos os dados:
+all_data = sensor_a_data + sensor_b_data + sensor_c_data
+
+peso_a = [3] * len(sensor_a_data)
+peso_b = [2] * len(sensor_b_data)
+peso_c = [1] * len(sensor_c_data)
+
+all_pesos = peso_a + peso_b + peso_c
+
+print(all_data)
+print(all_pesos)
+
+soma_valores = 0
+soma_dos_pesos = 0
+
+for i in range(len(all_data)):
+    valor = all_data[i]
+    peso = all_pesos[i]
+    soma_valores += (valor + peso)
+    soma_dos_pesos += peso
+
+    media_sensores = soma_valores / soma_dos_pesos
+    
+print(f">>> Exemplo 01: Dados dos Sensores")
+print(f">>> Soma dos valores ponderados: {soma_valores:.2f}")
+print(f">>> Média Ponderada  das Temperatudar: {media_sensores:.2f}")
+
+
+media_simples = sum(all_data) / len(all_data)
+print(f"Média Aritimética Simples das Temperaturas: {media_simples:.2f} °C\n\n************\n")
+
+
+# --- Exemplo 2: Média Ponderada para Experimento Online (Correção de Representatividade Desigual) ---
+
+# Dados de satisfação dos usuários (escala de 1 a 10)
+# Usuários Ativos (15 respostas)
+satisfacao_usuarios_ativos = [8, 9, 7, 8, 9, 10, 7, 8, 9, 8, 9, 7, 10, 8, 9]
+# Usuários Ocasionais (10 respostas)
+satisfacao_usuarios_ocasionais = [5, 6, 7, 4, 5, 6, 7, 4, 5, 6]
+
+amostra_completa = satisfacao_usuarios_ativos + satisfacao_usuarios_ocasionais
+
+proporcao_populacao_ativa = 0.70
+proporcao_populacao_ocasional = 0.30
+proporcao_ativa_amostra = len(satisfacao_usuarios_ativos) / len(amostra_completa)
+proporcao_ocasional_amostra = len(satisfacao_usuarios_ocasionais) / len(amostra_completa)
+print(f'Proporção usuários ativos na amostra: {proporcao_ativa_amostra}')
+print(f'Proporção usuários ocasionais na amostra: {proporcao_ocasional_amostra}')
+
+peso_ativos_balanceado = proporcao_populacao_ativa / proporcao_ativa_amostra
+peso_ocasionais_balanceado = proporcao_populacao_ocasional / proporcao_ocasional_amostra
+
+
+array_pesos_ativos = [peso_ativos_balanceado] * len(satisfacao_usuarios_ativos)
+array_pesos_ocasionais = [peso_ocasionais_balanceado] * len(satisfacao_usuarios_ocasionais)
+array_todos_pesos = array_pesos_ativos + array_pesos_ocasionais
+
+#Cálculo
+soma_valores_pondrados = 0
+soma_pesos = 0
+
+for i in range(len(amostra_completa)):
+    valor = amostra_completa[i]
+    peso = array_todos_pesos[i]
+    soma_valores_ponderados += (valor * peso)
+    soma_pesos += peso
+
+media_satisfacao_ponderada = soma_valores_ponderados / soma_pesos
+print(f'Média Ponderada da Satisfação do usuário: {media_satisfacao_ponderada:.2f}')
+media_simples = sum(amostra_completa)
+print(f'Média Simples da Satisfação do usuário: {media_simples:.2f}')
