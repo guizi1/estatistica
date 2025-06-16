a0c5b6b5ee3aa9c9975ee8e13b78e661fab822b6
EFG <efg@efg.local>
2025-06-16 19:55:00 -0300

    aula 16/06 quartis

--- percentis.py
+++ percentis.py
@@ -28,4 +28,26 @@ df_quartis = pd.DataFrame({
     'Quartil' : ['Primeiro (Q1)', 'Segundo (Q2 = Mediana)', 'Terceiro (Q3)'],
     'Valor' : [q1, q2, q3]
 })
+print('--- Quartis das Notas')
 print(df_quartis)
+print('\n')
+
+#--- calculos de decis
+decis_percentis = [i / 10.0 for i in range(1, 11)]
+decis_valores = [df_alunos['Nota'].quantile(p) for p in decis_percentis]
+decis_nomes = [f'{i}° Decil' for i in range(1, 11)]
+df_decis = pd.DataFrame({
+    'Decil': decis_nomes,
+    'Percentil': [f'{int(p*100)}%' for p in decis_percentis],
+    'Valor': decis_valores
+})
+print('--- Decis das Notas ---')
+print(df_decis)
+print('\n')
+
+amplitude_interquartil = q3 - q1
+df_aiq = pd.DataFrame({
+    'Métrica' : ['Amplitude Interquartil (AIQ)'],
+    'Valor': [amplitude_interquartil]
+})
+print(df_aiq)
