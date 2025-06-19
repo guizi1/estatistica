c409926e1f5ad6c7d5fdf4a034f9cb24b321f4d1
EFG <efg@efg.local>
2025-06-18 21:20:30 -0300

    Aula 18/06 amplitude

--- medias_homicidios.py
+++ medias_homicidios.py
@@ -1,5 +1,3 @@
-# .\venv\Scripts\activate
-# venv\Scripts\activate.sp1
 import pandas as pd
 import numpy as np
 # pip install scipy
@@ -27,6 +25,102 @@ def get_medias(df_dados_brutos):
     }, index=['Média','Média Aparada', 'Mediana', 'Media Ponderada'])
     return df_medias
 
-df_dados_brutos = pd.read_csv("taxa_homicidios.csv")
+df_dados_brutos = pd.read_csv('taxa_homicidios.csv')
 print(df_dados_brutos)
 df_medias = get_medias(df_dados_brutos)
+print(df_medias.to_string(float_format="%.2f"))
+
+def estimativas_variabilidade(dados_brutos, media):
+    """
+   Estimativas de Variabilidade
+   Indica o quão espalhados/dispersos os dados estão em relação ao centro (média, mediana, moda)
+      Desvios
+      Diferença entre os valores observados e uma estimativa de localização (média, mediana, moda)
+      - Desvio: tx_homicidio - media
+      - Desvio Absoluto: |tx_homicidio - media|
+      - Desvio Absoluto Médio: soma dos desvios / num desvios
+      - Variância: soma(desvio^2) / num desvios - 1
+      - Desvio Padrão: Raiz quadrada da variância
+      Estatísticas de Ordem
+      Estatísticas baseadas em dados ordenados (order)
+      - Amplitude: valor máximo - valor mínimo
+      - Percentil: Divide os valores em porcentagens           [10%, 20%, 30%, 40%, 50%, 60%, 70%, 80%, 90%, 100%]
+      - Quantil: Mesmo que percentil, mas com casas decimais   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+      - Quartil: Divide os valores em quatro partes iguais     [Q1 = 25%, Q2 = 50%, Q3 = 75%]
+      - Amplitude Interquartil: Q3 - Q1
+      - Mediana: Divide os valores em duas partes iguais       [med = Q2]
+         Percentis
+   """
+    print(estimativas_variabilidade.__doc__)
+    # ordenar os dados
+    dados_brutos = dados_brutos.sort_values()
+    # desvios
+    desvios = dados_brutos - media
+    # desvios absolutos
+    desvios_absolutos = np.abs(desvios)
+    # desvio absoluto medio
+    desvio_absoluto_medio = np.mean(desvios_absolutos)
+    # variancia (ddof = delta degreee of freedom)
+    variancia = np.var(dados_brutos, ddof=1)
+    # desvio padrão
+    desvio_padrao = np.std(dados_brutos, ddof=1)
+    # data frames
+    df_desvios_individuais = pd.DataFrame({
+        'Dados Brutos': dados_brutos,
+        'Desvio (taxa homicidio - média)': desvios,
+        'Desvio Absoluto |taxa homicidio - média|': desvios_absolutos
+    })
+    df_media_desvios = pd.DataFrame({
+        'Métrica Estatística': [
+            'Média',
+            'Desvio Padrão',
+            'Variância',
+            'Desvio Absoluto Médio (DAM)'
+        ],
+        'Valor Calculado': [
+            media,
+            desvio_padrao,
+            variancia,
+            desvio_absoluto_medio
+        ]
+    })
+    # imprimir dados
+    print(df_desvios_individuais.round(2))
+    print("\n")
+    print(df_media_desvios.round(2))
+    print("\n")
+    # quantil (decimais)
+    decimais = [0.05, 0.25, 0.5, 0.75, 0.95]
+    df_quantis = pd.DataFrame(dados_brutos.quantile(decimais))
+    print(df_quantis.transpose())
+    # percentil
+    df_percentis = pd.DataFrame(dados_brutos.quantile(decimais))
+    # novos_indices = []
+    # for p in decimais:
+    #    novos_indices.append(f'{p * 100}%')
+    # df_percentis.index = novos_indices
+    df_percentis.index = [f'{p  *100}%' for p in decimais]
+    print(df_percentis.transpose())
+    print('\n')
+
+    quartis = [0.25, 0.5, 0.75]
+    df_quartis = pd.DataFrame(dados_brutos.quantile(quartis))
+    df_quartis.index = ['Q1', 'Q2', 'Q3']
+    print(df_quartis.transpose())
+    print('\n')
+
+    amplitude = dados_brutos.max() - dados_brutos.min()
+    amplitude_alt = dados_brutos.iloc[-1] - dados_brutos.iloc[0]
+
+    amplitude_interquartil = df_quartis.max() - df_quartis.min()
+    amplitude_interquartil_alt = dados_brutos.quantile(0.75) - dados_brutos.quantile(0.25)
+    mediana = dados_brutos.median()
+    df_amplitudes = pd.DataFrame({
+        'Amplitude (max - min)': amplitude,
+        'Amplitude interquartil (Q3 - Q1)': amplitude_interquartil,
+        'Mediana (Q2)': mediana
+    })
+
+    print(df_amplitudes.round(2))
+
+estimativas_variabilidade(df_dados_brutos['Taxa homicidios'], np.mean(df_dados_brutos['Taxa homicidios']))
