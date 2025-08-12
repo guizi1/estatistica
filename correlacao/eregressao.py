895ab6e09bffc46d59405a7cffd74ca7a3fe347e
EFG <efg@efg.local>
2025-08-11 21:17:51 -0300

    Aula 11/08

--- correlacao/eregressao.py
+++ correlacao/eregressao.py
@@ -7,3 +7,18 @@ from sklearn.linear_model import LinearRegression
 EXPOSICAO_ALGODAO = 'data/LungDisease.csv'
 dataframe = pd.read_csv(EXPOSICAO_ALGODAO)
 print(dataframe.head())
+
+dataframe.plot.scatter(x = 'Exposure', y = 'PEFR')
+plt.show()
+
+predictors = ['Exposure']
+outcome = 'PEFR'
+model = LinearRegression()
+model.fit(dataframe[predictors], dataframe[outcome])
+
+print(f'Intercept: {model.intercept_:.3f}')
+print(f'Coeficiente Angular: {model.coef_[0]}')
+fig, (reg) = plt.subplots(1, 1, figsize = (4, 4))
+reg = sns.regplot(x = 'Exposure', y = 'PEFR', data = dataframe, ax = reg)
+plt.tight_layout()
+plt.show()
